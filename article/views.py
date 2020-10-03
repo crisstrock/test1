a1dc66cce79519879b1824bdea2cadeb594aee36
Cristian Ruiz <cristianruiz1195@gmail.com>
2020-10-02 19:29:47 -0500

    We create method to create and update articles

---
+++ article/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Article
+from .serializers import ArticleSerializer
+
+# Create your views here.
+class ArticleView(APIView):
+    def get(self, request):
+        articles = Article.objects.all()
+        # the many param informs the serializer that it will be serializing more than a single article.
+        serializer = ArticleSerializer(articles, many=True)
+        return Response({"articles": serializer.data})
+
+    def post(self, request):
+        article = request.data.get('article')
+
+        # Create an article from the above data
+        serializer = ArticleSerializer(data=article)
+        if serializer.is_valid(raise_exception=True):
+            article_saved = serializer.save()
+        return Response({"success": "Article '{}' created successfully".format(article_saved.title)})
+    
+    def put(self, request):
+        data = request.data.get('article')
+        saved_article = get_object_or_404(Article.objects.all(), title = data["title"])
+        serializer = ArticleSerializer(instance=saved_article, data=data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            article_saved = serializer.save()
+        return Response({"success": "Article '{}' updated successfully".format(article_saved.title)})
